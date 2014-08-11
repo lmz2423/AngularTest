de53cb062fcf3989629924c5c217e346945ea844
luo mingzhong <lmzpengpai@gmail.com>
2014-08-11 16:31:52 +0800

    show or hidde 文件

---
+++ js/cssExpression.js
@@ -0,0 +1,19 @@
+/**
+ * Created by creditease on 2014/8/5.
+ */
+function HeaderController($scope) {
+    $scope.isError = false;
+    $scope.isWarning = false;
+
+    $scope.showError = function(){
+        $scope.messageText = 'This is an error!';
+        $scope.isError = true;
+        $scope.isWarning = false;
+    };
+
+    $scope.showWarning = function() {
+        $scope.messageText = 'Just a warning . Please carry on.';
+        $scope.isWarning = true;
+        $scope.isError = false;
+    };
+}
--- js/showOrHide.js
+++ js/showOrHide.js
@@ -2,8 +2,13 @@
  * Created by creditease on 2014/8/5.
  */
 function DeathrayMenuController($scope) {
+    $scope.isDisabled = false;
     $scope.show = false;
     $scope.toggleMenu = function() {
         $scope.show = !$scope.show;
     };
+
+    $scope.stun = function(){
+        $scope.isDisabled = !$scope.isDisabled;
+    }
 }
