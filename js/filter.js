c22ad32badd5732a4f6e2e9839df55a0106af21c
luo mingzhong <lmzpengpai@gmail.com>
2015-02-04 11:35:55 +0800

    show or hidde 文件

---
+++ js/filter.js
@@ -0,0 +1,10 @@
+/**
+ * Created by creditease on 2015/1/29.
+ */
+(function () {
+    'use strict';
+    var filter = angular.module('FilterTest', []);
+    filter.controller("testFiler", function () {
+        console.log("xx");
+    });
+}());
