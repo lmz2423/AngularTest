8cdb52ea8c75c5953118995b89d9f8c304ff39d0
luo mingzhong <lmzpengpai@gmail.com>
2015-11-04 20:52:20 +0800

    show or hidde 文件

--- angular1.3.14/src/js/controller/Form.js
+++ angular1.3.14/src/js/controller/Form.js
@@ -4,15 +4,15 @@
 angular.module('UserInfoMoudle', ['ngRoute', 'ngAnimate'])
     .config(function ($routeProvider) {
         $routeProvider
-            .when('/PageOne',
-            {templateUrl: 'tpls/PageOne.html'})
+            .when('/PageOne', {templateUrl: 'tpls/PageOne.html'})
             .when('/Default', {templateUrl: 'tpls/form.html'})
             .when('/PageTwo', {templateUrl: 'tpls/PageTwo.html'})
 
             .otherwise({redirectTo: '/Default'})
     });
-angular.module('UserInfoMoudle').controller('UserCtrl', function ($scope) {
+angular.module('UserInfoMoudle').controller('UserCtrl', function ($scope,$location) {
     $scope.url = "http://10.106.2.111/kuaipan/Angular/angular1.3.14/src/index.html";
+    $location.path("/PageOne");
 });
 angular.module('UserInfoMoudle').controller('UserInfoCtrl', function ($scope) {
     $scope.userInfo = {
